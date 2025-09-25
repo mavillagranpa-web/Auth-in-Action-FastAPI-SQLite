b078f39be1e839b22d74962b81ce611a8cf23628
mavillagranpa-web <mavillagranpa@gmail.com>
2025-09-25 12:51:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
 from fastapi import FastAPI, HTTPException
-from sqlmodel import SQLModel, Field, create_engine, Session, select
+from sqlmodel import SQLModel
 
 app = FastAPI()
-engine = create_engine("sqlite:///database.db")
 
-class Usuario(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+# Usuario en memoria (aqui pudiera agregar una base de datos)
+class User(SQLModel):
     username: str
     password: str
 
-class LoginData(SQLModel):
+# Credenciales quemadas en código
+admin_user = User(username="naruto", password="rasengan123")
+
+class Credentials(SQLModel):
     username: str
     password: str
-
-@app.on_event("startup")
-def iniciar():
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        if not session.exec(select(Usuario).where(Usuario.username == "naruto")).first():
-            session.add(Usuario(username="naruto", password="rasengan123"))
-            session.commit()
 
 @app.get("/")
 def index():
-    return {"message": "Hola, darling"}
+    return {"message": "Hola, darling, has ingresado correctamente"}
 
 @app.post("/login")
-def login(data: LoginData):
-    with Session(engine) as session:
-        usuario = session.exec(select(Usuario).where(Usuario.username == data.username)).first()
-        if usuario and usuario.password == data.password:
-            return {"message": f"¡Bienvenido {usuario.username}! ⚡️"}
-        raise HTTPException(401, "Credenciales incorrectas")
+def login(credentials: Credentials):
+    if credentials.username == admin_user.username and credentials.password == admin_user.password:
+        return {"message": f"¡Bienvenido, darling :)❤️ {admin_user.username}! Tu chakra está listo ⚡️"}
+    raise HTTPException(401, "Usuario o contraseña incorrectos, revisa o contactate con el admin")
